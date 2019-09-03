24cbfe6086999d8d05e77e1f15a95e8e7afcc596
Blaz <34656869+blazsuhadolnik@users.noreply.github.com>
2019-09-04 00:44:54 +0200

    add some comment in code.

--- podanpisi.py
+++ podanpisi.py
@@ -1,24 +1,29 @@
-for j in range(1, 23):	
+#i have 22 episode in one telenovel so i repet code from 1 to 23 without number 23
+for j in range(1, 23):
+	#for every line in file (like 1.srt) save just lines with word Dialogue in array lines
 	lines = [line.rstrip('\n') for line in open(str(j)+'.srt') if line.find('Dialogue:') != -1]
+	#for array pod we just cut some start caracter in each line 
 	pod = []
 	for i in lines:
 		pod.append(i[12:])
-	casi = []
+	#array times is changing in shape with text and time seqence.
+	times = []
 	for i in pod:
-		casi.append(i.split(",", 8))
-	f=open('e'+str(j)+'.srt', 'a')
-	for i in range(1,len(casi)+1):
+		times.append(i.split(",", 8))
+	f=open('episode'+str(j)+'.srt', 'a')
+	for i in range(1,len(times)+1):
+		#writing in file
 		f.write(str(i))
 		f.write('\n')
-		f.write(casi[i-1][0] + "0 --> " + casi[i-1][1])
-		casi[i-1][-1] = casi[i-1][-1].replace('{\i1}', ' ')
-		casi[i-1][-1] = casi[i-1][-1].replace('{\i0}', ' ')
-		casi[i-1][-1] = casi[i-1][-1].replace('\\N', '\n')
+		#writing time
+		f.write(times[i-1][0] + "0 --> " + times[i-1][1])
+		#removing some sequence in text before writing in file.
+		times[i-1][-1] = times[i-1][-1].replace('{\i1}', ' ')
+		times[i-1][-1] = times[i-1][-1].replace('{\i0}', ' ')
+		times[i-1][-1] = times[i-1][-1].replace('\\N', '\n')
+		#writting text in file
 		f.write('\n')
-		f.write(casi[i-1][-1])
+		f.write(times[i-1][-1])
 		f.write('\n\n')
+	#close file
 	f.close()
-
-
-#for i in casi:
-#	print(i)
